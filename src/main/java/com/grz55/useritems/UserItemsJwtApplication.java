df37b3f99e533b48237ac420550388359ba7b53f
grz55 <grz55@vp.pl>
2025-02-07 15:28:24 +0100

    Project configuration initial commit

---
+++ src/main/java/com/grz55/useritems/UserItemsJwtApplication.java
@@ -0,0 +1,13 @@
+package com.grz55.useritems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserItemsJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserItemsJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grz55/useritems/UserItemsJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grz55.useritems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserItemsJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
