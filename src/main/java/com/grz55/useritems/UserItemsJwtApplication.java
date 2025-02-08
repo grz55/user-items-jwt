c140a4fe5521cb14d498a3faefcd0a3df3e031d5
grz55 <grz55@vp.pl>
2025-02-08 16:06:30 +0100

    Reformat code

--- src/main/java/com/grz55/useritems/UserItemsJwtApplication.java
+++ src/main/java/com/grz55/useritems/UserItemsJwtApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserItemsJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserItemsJwtApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UserItemsJwtApplication.class, args);
+  }
 }
--- src/test/java/com/grz55/useritems/UserItemsJwtApplicationTests.java
+++ src/test/java/com/grz55/useritems/UserItemsJwtApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserItemsJwtApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
